102871ca930a00551a888efa11c664c7d3409d3f
John T. Wodder II <git@varonathe.org>
2019-04-13 23:12:46 +0000

    Test marshalling bool, bytes, and "\N"

--- test/test_marshal.py
+++ test/test_marshal.py
@@ -1,6 +1,6 @@
-from   dbcsv import marshal_object, unmarshal_object
 import pytest
 import sqlalchemy as S
+from   dbcsv import marshal_object, unmarshal_object
 
 schema = S.MetaData()
 
@@ -9,12 +9,28 @@ table = S.Table('table', schema,
     S.Column('name', S.Unicode(2048)),
     S.Column('str', S.String(32)),
     S.Column('about', S.UnicodeText),
+    S.Column('data', S.LargeBinary),
+    S.Column('truth', S.Boolean),
 )
 
 @pytest.mark.parametrize('dbtyped,strtyped', [
     (
-        {"id": None, "name": None, "str": None, "about": None},
-        {"id": r'\N', "name": r'\N', "str": r'\N', "about": r'\N'},
+        {
+            "id": None,
+            "name": None,
+            "str": None,
+            "about": None,
+            "data": None,
+            "truth": None,
+        },
+        {
+            "id": r'\N',
+            "name": r'\N',
+            "str": r'\N',
+            "about": r'\N',
+            "data": r'\N',
+            "truth": r'\N',
+        },
     ),
 
     (
@@ -23,14 +39,22 @@ table = S.Table('table', schema,
             "name": "user",
             "str": "thing",
             "about": "This is test text.",
+            "data": b'\xDE\xAD\xBE\xEF',
+            "truth": True,
         },
         {
             "id": "1",
             "name": "user",
             "str": "thing",
             "about": "This is test text.",
+            "data": "3q2+7w==",
+            "truth": "t",
         },
     ),
+
+    ({"str": r'\N'}, {"str": r'\\N'}),
+    ({"str": r'\\N'}, {"str": r'\\\\N'}),
+    ({"truth": False}, {"truth": "f"}),
 ])
 def test_marshal_object(dbtyped, strtyped):
     assert marshal_object(table, dbtyped) == strtyped
