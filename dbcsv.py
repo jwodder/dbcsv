aca83babd2c52de196adb3808bccb8deaabcfdf4
John T. Wodder II <git@varonathe.org>
2019-04-13 22:43:08 +0000

    Packaging

--- dbcsv.py
+++ dbcsv.py
@@ -1,3 +1,15 @@
+"""
+Dump & load databases as CSV
+
+Visit <https://github.com/jwodder/dbcsv> for more information.
+"""
+
+__version__      = '0.1.0.dev1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'dbcsv@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/dbcsv'
+
 import base64
 import csv
 from   pathlib import Path
